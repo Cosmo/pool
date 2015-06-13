e71768404cfbe6fc243333ddf3fe732bc15f5df5
Devran Ünal <devran@me.com>
2015-06-13 18:49:02 +0200

    update something else

---
+++ Pool/Pool/ViewControllers/ActivityDetailTableViewController.swift
@@ -0,0 +1,83 @@
+//
+//  ActivityDetailTableViewController.swift
+//  
+//
+//  Created by Devran Uenal on 13.06.15.
+//
+//
+
+import UIKit
+
+class ActivityDetailTableViewController: UITableViewController {
+    private var _id: String?
+    
+    var data: Activity?
+    
+    let transactionCell = "transactionCell"
+    
+    convenience init(id: String) {
+        self.init()
+        self._id = id
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = "Details"
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Invite", style: UIBarButtonItemStyle.Plain, target: self, action: "invite:")
+        
+        if let id = self._id {
+            Activity.detail(id)?.success({ (data, response) -> () in
+                var stringData = NSString(data: data, encoding: NSUTF8StringEncoding)! as String
+                self.data <-- stringData
+                println(self.data)
+                dispatch_async(dispatch_get_main_queue(), {
+                    self.tableView.reloadData()
+                })
+            }).failure({ (data, response, error) -> () in
+                println("failure")
+            }).call()
+        }
+    }
+    
+    func invite(sender: AnyObject) {
+        let viewController = UINavigationController(rootViewController: InviteTableViewController())
+        self.presentViewController(viewController, animated: true, completion: nil)
+    }
+
+    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 2
+    }
+
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+            if let count = self.data?.transactions?.count {
+                return count
+            } else {
+                return 0
+            }
+        case 1:
+            if let count = self.data?.users?.count {
+                return count
+            } else {
+                return 0
+            }
+        default:
+            return 0
+        }
+    }
+    
+    override func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Transactions"
+        case 1:
+            return "People"
+        default:
+            return ""
+        }
+    }
+    
+    
+}
