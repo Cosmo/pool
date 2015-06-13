a17d8dd390ec1f28fcf123dd4cd3f43948bddad2
Kai Mysliwiec <kai.mysliwiec@prosiebensat1digital.de>
2015-06-13 13:28:31 +0200

    mongodb connection and Activity schema

--- node/index.js
+++ node/index.js
@@ -1,7 +1,28 @@
 var restify = require('restify');
+var mongoose = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+switch (process.env.NODE_ENV) {
+  case 'development':
+    mongoose.connect('mongodb://localhost/pool');
+    break;
+  default:
+    mongoose.connect('mongodb://pool:tzOc4gHSXHJcyGwTCn6WZcW4_2c.M6_C5JmEpzk9uyA-@ds036178.mongolab.com:36178/pool');
+
+}
+
+var activitySchema = new Schema({
+  id:       Schema.ObjectId,
+  name:     String,
+  master:   String
+});
+var Activity = mongoose.model('Activity', activitySchema);
+
+Activity.create({name: 'Burda Hackday', master: 'kaimys'});
 
 var server = restify.createServer({
-  name: 'myapp',
+  name: 'pool-backend',
   version: '1.0.0'
 });
 server.use(restify.acceptParser(server.acceptable));
@@ -11,6 +32,17 @@ server.use(restify.bodyParser());
 server.get('/echo/:name', function (req, res, next) {
   res.send(req.params);
   return next();
+});
+
+server.get('/activities', function (req, res, next) {
+  Activity.find({}, function (err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send(docs);
+      return next();
+    }
+  });
 });
 
 server.listen(8080, function () {
