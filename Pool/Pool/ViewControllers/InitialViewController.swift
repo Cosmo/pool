4e0d1e28ed770d36bd4ad7e357a1db139ef987ae
Devran Ünal <devran@me.com>
2015-06-13 16:37:02 +0200

    ..

--- Pool/Pool/ViewControllers/InitialViewController.swift
+++ Pool/Pool/ViewControllers/InitialViewController.swift
@@ -9,43 +9,7 @@
 import UIKit
 
 class InitialViewController: UITableViewController {
-    var data = [Activity]()
-    let activityCell = "activityCell"
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.title = "Pool"
-        
-        self.tableView.registerClass(ActivityTableViewCell.self, forCellReuseIdentifier: activityCell)
-        
-        Activity.all()?.success({ (data, response) -> () in
-            var stringData = NSString(data: data, encoding: NSUTF8StringEncoding)! as String
-            self.data <-- stringData
-            dispatch_async(dispatch_get_main_queue(), {
-                self.tableView.reloadData()
-            })
-        }).failure({ (data, response, error) -> () in
-            println("failure")
-        }).call()
-    }
-
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.data.count
-    }
-
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        
-        var cell: ActivityTableViewCell
-        cell = tableView.dequeueReusableCellWithIdentifier(activityCell, forIndexPath: indexPath) as! ActivityTableViewCell
-        
-        cell.textLabel?.text = self.data[indexPath.row].name
-
-        return cell
-    }
     /*
 
         // navigationBar
