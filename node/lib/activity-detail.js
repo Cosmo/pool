3c6a9f5185077638a2a429a27f4c001537465dc2
Olcay Buyan <olcay@buyan.de>
2015-06-13 19:17:57 +0200

    users property added

--- node/lib/activity-detail.js
+++ node/lib/activity-detail.js
@@ -14,11 +14,30 @@ function activityDetail(req, res, next) {
           console.log(err);
           next(err);
         } else {
+
+            // users = [{name:"donnie",amount:250},{name:"thomas",amount:-110},...]
+            // transactions = [{amount:290,fee:3,currency:"usd",user:"donnie"},...]
+
+            var users = Enumerable.From(transactions)
+                .GroupBy("{ name: $.name }", null,
+                    function (key, g) {
+                        var result = {
+                            name: key.name,
+                            paidTotal: g.Sum($.amount)
+                        }
+                    });
+
+            var amountTotal = Enumerable.From(transactions).Sum(function (t) { return t.amount });
+
+            users.ForEach(function(u) {
+                u.amount = (((amountTotal / users.length) - u.amount) * -1);
+            });
+          
           var newActivity = {
             _id: activity._id,
             name: activity.name,
             master: activity.master,
-            users: [],
+            users: users,
             transactions: transactions
           };
           res.send(newActivity);
