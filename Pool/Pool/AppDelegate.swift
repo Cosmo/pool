5629b7fc0f37c4a479452a77c7cfb9506fecc035
Thomas Pockrandt <me@thomaspockrandt.com>
2015-06-13 14:55:45 +0200

    Add initial stuff

--- Pool/Pool/AppDelegate.swift
+++ Pool/Pool/AppDelegate.swift
@@ -10,12 +10,14 @@ import UIKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
     var window: UIWindow?
-
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window!.rootViewController = UINavigationController(rootViewController: InitialViewController())
+        self.window!.makeKeyAndVisible()
+        
         return true
     }
 
