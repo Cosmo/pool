c0f9289cf951d65d5df99e0da1a7d3de7a5d4c8a
Thomas Pockrandt <me@thomaspockrandt.com>
2015-06-13 15:08:28 +0200

    Use real twitter handles #2

--- node/lib/fixtures.js
+++ node/lib/fixtures.js
@@ -3,52 +3,52 @@ module.exports = {
     {
       id: 3423490823,
       name: "Burda Hackathon",
-      master: "cosmo",
+      master: "maccosmo",
       users: [
-        { name: "donnieray", amount: 500, currency: "eur" },
-        { name: "thomazr", amount: -250, currency: "eur" },
-        { name: "olcay", amount: -250, currency: "eur" },
-        { name: "cosmo", amount: 45, currency: "eur" },
+        { name: "donnieraycrisp", amount: 500, currency: "eur" },
+        { name: "thomaspockrandt", amount: -250, currency: "eur" },
+        { name: "olcaybuyan", amount: -250, currency: "eur" },
+        { name: "maccosmo", amount: 45, currency: "eur" },
       ]
     },
     {
       id: 3423490823,
       name: "USA Reise",
-      master: "olcay",
+      master: "olcaybuyan",
       users: [
-        { name: "donnieray", amount: 500, currency: "eur" },
-        { name: "thomazr", amount: -250, currency: "eur" },
-        { name: "olcay", amount: -250, currency: "eur" },
-        { name: "cosmo", amount: 45, currency: "eur" },
+        { name: "donnieraycrisp", amount: 500, currency: "eur" },
+        { name: "thomaspockrandt", amount: -250, currency: "eur" },
+        { name: "olcaybuyan", amount: -250, currency: "eur" },
+        { name: "maccosmo", amount: 45, currency: "eur" },
       ]
     },
     {
       id: 3423490823,
       name: "Roadtrip",
-      master: "donnieray",
+      master: "donnieraycrisp",
       users: [
-        { name: "donnieray", amount: 500, currency: "eur" },
-        { name: "thomazr", amount: -250, currency: "eur" },
-        { name: "olcay", amount: -250, currency: "eur" },
-        { name: "cosmo", amount: 45, currency: "eur" },
+        { name: "donnieraycrisp", amount: 500, currency: "eur" },
+        { name: "thomaspockrandt", amount: -250, currency: "eur" },
+        { name: "olcaybuyan", amount: -250, currency: "eur" },
+        { name: "maccosmo", amount: 45, currency: "eur" },
       ]
     }
   ],
 
   activity: {
     id: 3423490823,
-    master: "olcay",
+    master: "olcaybuyan",
     name: "USA Reise",
     users: [
-      { name: "donnieray", amount: 500, currency: "eur" },
-      { name: "thomazr", amount: -250, currency: "eur" },
-      { name: "olcay", amount: -250, currency: "eur" },
-      { name: "cosmo", amount: 45, currency: "eur" },
+      { name: "donnieraycrisp", amount: 500, currency: "eur" },
+      { name: "thomaspockrandt", amount: -250, currency: "eur" },
+      { name: "olcaybuyan", amount: -250, currency: "eur" },
+      { name: "maccosmo", amount: 45, currency: "eur" },
     ],
     transactions: [
-      { amount: 2900, fee: 0,   currency: "usd", user: "donnieray" },
-      { amount: 999,  fee: 150, currency: "eur", user: "thomazr" },
-      { amount: 350,  fee: 150, currency: "usd", user: "cosmo" },
+      { amount: 2900, fee: 0,   currency: "usd", user: "donnieraycrisp" },
+      { amount: 999,  fee: 150, currency: "eur", user: "thomaspockrandt" },
+      { amount: 350,  fee: 150, currency: "usd", user: "maccosmo" },
     ],
     total: {
       amount: 4899,
